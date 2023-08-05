e97d8271a753ed0a0b0116f3948ccbcdc1ecd3f5
Julkar Nime <julkarnime09@gmail.com>
2023-08-05 06:09:33 +0600

    Created ASP.NET Core MVC Project

---
+++ AlMuzainiCMS/AlMuzainiCMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlMuzainiCMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
