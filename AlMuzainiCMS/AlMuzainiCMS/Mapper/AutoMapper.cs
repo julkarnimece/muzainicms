ee62d40267d54c0cc9ab986868114c2e5c8b07b1
Julkar Nime <julkarnime09@gmail.com>
2023-08-05 21:11:39 +0600

    added packages

---
+++ AlMuzainiCMS/AlMuzainiCMS/Mapper/AutoMapper.cs
@@ -0,0 +1,19 @@
+﻿using AlMuzainiCMS.Models.LoginVM;
+using AlMuzainiCMS.Models.Models;
+using AutoMapper;
+
+namespace AlMuzainiCMS.Mapper
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<UserInfo, UserInfoVM>();
+            CreateMap<UserInfoVM, UserInfo>();
+
+
+        }
+
+
+    }
+}
